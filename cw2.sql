e17f2f7ebbed0727bc228047abf39b6a23cece10
MartynaJarosz <101349241+MartynaJarosz@users.noreply.github.com>
2022-10-23 19:44:39 +0200

    Add files via upload

---
+++ cw2.sql
@@ -0,0 +1,32 @@
+--Zadanie 5
+CREATE TABLE airportsNew AS
+(SELECT name, geom, elev FROM airports)
+
+--a) 
+SELECT name as westAirport, ST_X(geom)
+FROM airportsNew
+ORDER BY ST_X(geom) DESC 
+LIMIT 1;
+
+SELECT name as eastAirport, ST_X(geom)
+FROM airportsNew
+ORDER BY ST_X(geom)
+LIMIT 1;
+
+--b) 
+INSERT INTO airportsnew(name, geom, elev) VALUES
+('airportB',
+	(SELECT ST_Centroid(ST_Makeline (
+			(SELECT geom FROM airportsNew WHERE name LIKE 'ANNETTE ISLAND'), 
+			(SELECT geom FROM airportsNew WHERE name LIKE 'ATKA')))),210
+);
+--Zadanie 6
+SELECT ST_Area(ST_Buffer(ST_ShortestLine(lakes.geom,airportsNew.geom),1000)) 
+FROM lakes, airportsNew
+WHERE lakes.names = 'Iliamna Lake' AND airportsNew.name='AMBLER';
+
+--Zadanie 7
+SELECT SUM(ST_Area(trees.geom)), trees.vegdesc
+FROM trees, tundra, swamp
+WHERE ST_Within(trees.geom,tundra.geom) OR ST_Within(trees.geom,swamp.geom)
+GROUP BY trees.vegdesc
