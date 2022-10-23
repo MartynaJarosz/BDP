aae73aa78cb1679aedc7248c092ce86fc8f033c7
MartynaJarosz <101349241+MartynaJarosz@users.noreply.github.com>
2022-10-23 23:22:19 +0200

    Update cw2.sql

--- cw2.sql
+++ cw2.sql
@@ -12,25 +12,23 @@ CREATE TABLE airportsNew AS
 (SELECT name, geom, elev FROM airports)
 
 --a) 
-SELECT name as westAirport, ST_X(geom)
-FROM airportsNew
+SELECT name as zachodAirport, ST_X(geom) FROM airportsNew
 ORDER BY ST_X(geom) DESC 
 LIMIT 1;
 
-SELECT name as eastAirport, ST_X(geom)
-FROM airportsNew
+SELECT name as wschodAirport, ST_X(geom) FROM airportsNew
 ORDER BY ST_X(geom)
 LIMIT 1;
 
 --b) 
-INSERT INTO airportsnew(name, geom, elev) VALUES
+INSERT INTO airportsNew(name, geom, elev) VALUES
 ('airportB',
 	(SELECT ST_Centroid(ST_Makeline (
 			(SELECT geom FROM airportsNew WHERE name LIKE 'ANNETTE ISLAND'), 
-			(SELECT geom FROM airportsNew WHERE name LIKE 'ATKA')))), 200
-);
+			(SELECT geom FROM airportsNew WHERE name LIKE 'ATKA')))), 200);
+			
 --Zadanie 6
-SELECT ST_Area(ST_Buffer(ST_ShortestLine(lakes.geom,airportsNew.geom),1000)) 
+SELECT ST_Area(ST_Buffer(ST_ShortestLine(lakes.geom,airportsNew.geom), 1000)) 
 FROM lakes, airportsNew
 WHERE lakes.names = 'Iliamna Lake' AND airportsNew.name='AMBLER';
 
@@ -38,4 +36,4 @@ WHERE lakes.names = 'Iliamna Lake' AND airportsNew.name='AMBLER';
 SELECT SUM(ST_Area(trees.geom)), trees.vegdesc
 FROM trees, tundra, swamp
 WHERE ST_Within(trees.geom,tundra.geom) OR ST_Within(trees.geom,swamp.geom)
-GROUP BY trees.vegdesc
+GROUP BY trees.vegdesc;
